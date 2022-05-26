75d39fb18bde73a1bf51bdf0b05686a5057996d9
matthieu-ortlieb <matthieu.ortlieb@cafefrappe.fr>
2022-05-26 11:03:44 +0200

    closes #4, closes #6, closes #7; + install procedure in README, Entity creation, database setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BileMo
 BileMo API
+
+-----
+### 1.Clone the repository
+run `https://github.com/Ma77hieu/BileMo.git`
+
+### 2.Install required libraries
+run `composer install`
+
+### 3.Configure your database inside the .env file
+place the correct values in the DATABASE_URL constant of the .env file
+
+### 4.Create your database
+run `php bin/console doctrine:database:create`
+
+### 5.Apply the project migrations
+in order to create the database tables, run
+`php bin/console doctrine:migrations:migrate`
---
+++ src/Entity/Product.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $brand;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $model;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $version;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $color;
+
+    #[ORM\Column(type: 'decimal', scale: 2)]
+    private $price;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
+
+    public function setVersion(?string $version): self
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
